e5cf763cb75ca5523064dd953c87e91f21e31005
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-06-20 15:29:11 +0800

    Initial commit

---
+++ src/main/java/com/springboot/springsecurityregistration/SpringsecurityregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurityregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityregistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityregistrationApplication.class, args);
+    }
+
+}
